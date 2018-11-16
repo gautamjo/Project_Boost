e405ba8f3e2a0c3a13878c91be0662ef3b968c4c
gigaelectron <gautam_joshi01@yahoo.com>
2018-11-17 00:19:27 +0530

    RocketShip complete and test flight with audio

---
+++ Assets/RocketShip.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketShip : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        // Fetch RigidBody from the GameObject
+        rigidBody = GetComponent<Rigidbody>();
+        // Fetch AudioSource from the GameObject
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) { // so the audio dosen't layer on 
+                                          //top of eachother.
+                audioSource.Play();
+            }
+        }
+
+        else {
+            audioSource.Pause();
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(Vector3.forward);
+        }
+
+        else if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(-Vector3.forward);
+        }
+
+    }
+}

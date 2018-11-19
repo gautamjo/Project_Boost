aae88947ac0ebfa86faa4292591fcb46fa428bec
gigaelectron <gautam_joshi01@yahoo.com>
2018-11-19 19:45:31 +0530

    Levels, sounds and functionality added

--- Assets/RocketShip.cs
+++ Assets/RocketShip.cs
@@ -2,16 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RocketShip : MonoBehaviour {
 
     // Setup default reaction controll system (rcs)
     [SerializeField] float rcsThrust = 100f;
+    // setup default main thrust system
     [SerializeField] float mainThrust = 100f;
-
+    // Setup audio clip
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip Success;
+    [SerializeField] AudioClip Death;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State {Alive, Dying, Transcending}
+    State state = State.Alive;
 
 	// Use this for initialization
 	void Start () {
@@ -23,18 +31,57 @@ public class RocketShip : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
+
+        if (state == State.Alive) {
+            ProcessInput();
+        }
 	}
 
     void OnCollisionEnter (Collision collision)
     {
-        if (collision.gameObject.tag == "Friendly") {
-            print("You're Ok");
+        if (state != State.Alive) {
+            return;
         }
 
-        else {
-            print("You're Dead");
+        if (collision.gameObject.tag == "Friendly") {
+            //print("You're Ok");
         }
+
+        else if (collision.gameObject.tag == "Finish")
+        {
+            StartSuccessSequence();
+        }
+
+        else
+        {
+            StartDeathSequence();
+        }
+    }
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(Success);
+        Invoke("LoadNextScene", 1f); // parameterize time
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(Death);
+        Invoke("LoadFirstLevel", 1f); // parameterize time 
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1); // todo allow for more than 2 levels
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
     }
 
     private void ProcessInput()
@@ -48,17 +95,22 @@ public class RocketShip : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            if (!audioSource.isPlaying)
-            { // so the audio dosen't layer on 
-              //top of eachother.
-                audioSource.Play();
-            }
+            ApplyThrust();
         }
 
         else
         {
             audioSource.Pause();
+        }
+    }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying)
+        { // so the audio dosen't layer on 
+          //top of eachother.
+            audioSource.PlayOneShot(mainEngine);
         }
     }
 
